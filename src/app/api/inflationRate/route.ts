c0c82edb4aa2728b5c0e5d8d78d84979f6187895
Franco Lorenzo <franco14lorenzo@gmail.com>
2023-06-11 20:57:58 -0300

    refactor: add env example

--- src/app/api/inflationRate/route.ts
+++ src/app/api/inflationRate/route.ts
@@ -1,13 +1,11 @@
 import { NextResponse } from 'next/server'
 
-const token =
-  process.env.BCRA_TOKEN ||
-  'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTc0NDkyNTEsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJmcmFuY28xNGxvcmVuem9AZ21haWwuY29tIn0.lUkKbY2XjVCYg5p2af4SLECtN1c9WKL7W6GVvKk5Hh5-_TOmR4LA5bIF8mRTMr_qH5LWeJOxCp8fJE7yz9Tn0g'
+const token = process.env.BCRA_TOKEN
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const date = searchParams.get('date')
-  console.log(date)
+
   const response = await fetch(
     'https://api.estadisticasbcra.com/inflacion_mensual_oficial',
     {
@@ -18,8 +16,6 @@ export async function GET(request: Request) {
   )
 
   const data = await response.json()
-
-  console.log(data[0].d)
 
   const filteredData = data.filter((x: any) => x.d.startsWith(date))
 
